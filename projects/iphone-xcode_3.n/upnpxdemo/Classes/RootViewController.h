f19ebd6694bc826c2a8a28e6a99e111a372cf9dd
Bruno Keymolen <bruno.keymolen@gmail.com>
2011-05-23 02:04:57 +0000

    added xcode 3.n iPhone upnpx demo project

---
+++ projects/iphone-xcode_3.n/upnpxdemo/Classes/RootViewController.h
@@ -0,0 +1,14 @@
+//
+//  RootViewController.h
+//  upnpxdemo
+//
+//  Created by Bruno Keymolen on 22/05/11.
+//  Copyright Bruno Keymolen 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+}
+
+@end
