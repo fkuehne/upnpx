001b62bc82c9f0c8f58ae317319eff654a6462f6
Bruno Keymolen <bruno.keymolen@gmail.com>
2011-05-29 01:11:34 +0000

    Adding Telephony:1

--- src/api/SoapActionsMessaging1.h
+++ src/api/SoapActionsMessaging1.h
@@ -10,5 +10,23 @@
 
 //SOAP
 
+-(int)GetTelephonyIdentityWithOutTelephonyIdentity:(NSMutableString*)telephonyidentity;
+-(int)GetMessagingCapabilitiesWithOutSupportedCapabilities:(NSMutableString*)supportedcapabilities;
+-(int)GetNewMessagesWithOutNewMessages:(NSMutableString*)newmessages;
+-(int)SearchMessagesWithMessageClass:(NSString*)messageclass MessageFolder:(NSString*)messagefolder MessageStatus:(NSString*)messagestatus SessionID:(NSString*)sessionid OutMessageList:(NSMutableString*)messagelist;
+-(int)ReadMessageWithMessageID:(NSString*)messageid OutMessageRequested:(NSMutableString*)messagerequested;
+-(int)SendMessageWithMessageToSend:(NSString*)messagetosend OutMessageID:(NSMutableString*)messageid;
+-(int)DeleteMessageWithMessageID:(NSString*)messageid;
+-(int)CreateSessionWithSessionClass:(NSString*)sessionclass SessionRecipients:(NSString*)sessionrecipients Subject:(NSString*)subject SupportedContentType:(NSString*)supportedcontenttype OutSessionID:(NSMutableString*)sessionid;
+-(int)ModifySessionWithSessionID:(NSString*)sessionid SessionRecipientsToAdd:(NSString*)sessionrecipientstoadd SessionRecipientsToRemove:(NSString*)sessionrecipientstoremove Subject:(NSString*)subject SupportedContentType:(NSString*)supportedcontenttype SessionClass:(NSString*)sessionclass;
+-(int)AcceptSessionWithSessionID:(NSString*)sessionid;
+-(int)GetSessionUpdatesWithOutSessionUpdates:(NSMutableString*)sessionupdates;
+-(int)GetSessionsWithSessionID:(NSString*)sessionid SessionClass:(NSString*)sessionclass SessionStatus:(NSString*)sessionstatus OutSessionsList:(NSMutableString*)sessionslist;
+-(int)JoinSessionWithSessionID:(NSString*)sessionid;
+-(int)LeaveSessionWithSessionID:(NSString*)sessionid;
+-(int)CloseSessionWithSessionID:(NSString*)sessionid;
+-(int)StartFileTransferWithFileInfoList:(NSString*)fileinfolist;
+-(int)CancelFileTransferWithSessionID:(NSString*)sessionid;
+-(int)GetFileTransferSessionWithSessionID:(NSString*)sessionid OutFileInfoList:(NSMutableString*)fileinfolist;
 
 @end
