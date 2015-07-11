345fd31e7248e49e42dcda8400cb48288448a42c
Felix Paul Kühne <fkuehne@videolan.org>
2015-07-11 16:56:39 +0200

    Basic Parser: fix runtime exception

--- src/upnp/BasicParser.m
+++ src/upnp/BasicParser.m
@@ -183,10 +183,13 @@ static NSString *ElementStop = @"ElementStop";
         NSData *data = [NSData dataWithContentsOfURL:url];
         if (data != nil) {
             NSString *xml = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
-            NSError *error = NULL;
-            NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\s*$\\r?\\n" options:NSRegularExpressionAnchorsMatchLines error:&error];
-            xml = [regex stringByReplacingMatchesInString:xml options:0 range:NSMakeRange(0, [xml length]) withTemplate:@""];
-            data = [xml dataUsingEncoding:NSUTF8StringEncoding];
+            if (xml != nil) {
+                NSError *error = NULL;
+                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\s*$\\r?\\n" options:NSRegularExpressionAnchorsMatchLines error:&error];
+                xml = [regex stringByReplacingMatchesInString:xml options:0 range:NSMakeRange(0, [xml length]) withTemplate:@""];
+                data = [xml dataUsingEncoding:NSUTF8StringEncoding];
+            } else
+                return -1;
         }
         NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
         int ret = [self startParser:parser];
