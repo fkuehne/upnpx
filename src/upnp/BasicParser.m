a8cd0b90d47ef1549d33883647af50c9bf5f85be
Alexey Lizenko <alizenko@cogniance.com>
2016-06-29 15:30:55 +0300

    Memory management improvements: reduce memory footprint on large media server folders
    Signed-off-by: Felix Paul Kühne <fkuehne@videolan.org>

--- src/upnp/BasicParser.m
+++ src/upnp/BasicParser.m
@@ -94,86 +94,97 @@ static NSString *ElementStop = @"ElementStop";
     NSArray *elementStack;
     NSArray *assets;
 
-    @synchronized (self) {
-        elementStack = [[stack copy] autorelease];
-        assets = [[mAssets copy] autorelease];
-    }
-    NSEnumerator *enumer = [assets objectEnumerator];
-    while((asset = [enumer nextObject])){
-        //Full compares go first
-        if([[asset path] isEqualToArray:elementStack]){
-            ret = asset;
-            break;
-        }else{
-            // * -> leafX -> leafY
-            //Maybe we have a wildchar, that means that the path after the wildchar must match
-            if([(NSString*)[asset path][0] isEqualToString:@"*"]){
-                if([elementStack count] >= [[asset path] count]){
-                    //Path ends with
-                    NSMutableArray *lastStackPath = [[NSMutableArray alloc] initWithArray:elementStack];
-                    NSMutableArray *lastAssetPath = [[NSMutableArray alloc] initWithArray:[asset path]];
-                    //cut the * from our asset path
-                    [lastAssetPath removeObjectAtIndex:0];
-                    //make our (copy of the) curents stack the same length
-                    NSUInteger elementsToRemove = [lastStackPath count] - [lastAssetPath count];
-                    NSRange range;
-                    range.location = 0;
-                    range.length = elementsToRemove;
-                    [lastStackPath removeObjectsInRange:range];
-                    if([lastAssetPath isEqualToArray:lastStackPath]){
-                        ret = asset;
+    @autoreleasepool {
+        @synchronized (self) {
+            elementStack = [[stack copy] autorelease];
+            assets = [[mAssets copy] autorelease];
+        }
+        NSEnumerator *enumer = [assets objectEnumerator];
+        while((asset = [enumer nextObject])){
+            //Full compares go first
+            if([[asset path] isEqualToArray:elementStack]){
+                ret = asset;
+                break;
+            }else{
+                // * -> leafX -> leafY
+                //Maybe we have a wildchar, that means that the path after the wildchar must match
+                if([(NSString*)[asset path][0] isEqualToString:@"*"]){
+                    if([elementStack count] >= [[asset path] count]){
+                        //Path ends with
+                        NSMutableArray *lastStackPath = [[NSMutableArray alloc] initWithArray:elementStack];
+                        NSMutableArray *lastAssetPath = [[NSMutableArray alloc] initWithArray:[asset path]];
+                        //cut the * from our asset path
+                        [lastAssetPath removeObjectAtIndex:0];
+                        //make our (copy of the) curents stack the same length
+                        NSUInteger elementsToRemove = [lastStackPath count] - [lastAssetPath count];
+                        NSRange range;
+                        range.location = 0;
+                        range.length = elementsToRemove;
+                        [lastStackPath removeObjectsInRange:range];
+                        if([lastAssetPath isEqualToArray:lastStackPath]){
+                            ret = asset;
+                            [lastAssetPath release];
+                            [lastStackPath release];
+                            break;
+                        }
                         [lastAssetPath release];
                         [lastStackPath release];
-                        break;
                     }
-                    [lastAssetPath release];
-                    [lastStackPath release];
                 }
-            }
-            // leafX -> leafY -> *
-            if([(NSString*)[[asset path] lastObject] isEqualToString:@"*"]){
-                if([elementStack count] == [[asset path] count] && [elementStack count] > 1){
-                    //Path start with
-                    NSMutableArray *beginStackPath = [[NSMutableArray alloc] initWithArray:elementStack];
-                    NSMutableArray *beginAssetPath = [[NSMutableArray alloc] initWithArray:[asset path]];
-                    //Cut the last entry (which is * in one array and <element> in the other
-                    [beginStackPath removeLastObject];
-                    [beginAssetPath removeLastObject];
-                    if([beginAssetPath isEqualToArray:beginStackPath]){
-                        ret = asset;
+                // leafX -> leafY -> *
+                if([(NSString*)[[asset path] lastObject] isEqualToString:@"*"]){
+                    if([elementStack count] == [[asset path] count] && [elementStack count] > 1){
+                        //Path start with
+                        NSMutableArray *beginStackPath = [[NSMutableArray alloc] initWithArray:elementStack];
+                        NSMutableArray *beginAssetPath = [[NSMutableArray alloc] initWithArray:[asset path]];
+                        //Cut the last entry (which is * in one array and <element> in the other
+                        [beginStackPath removeLastObject];
+                        [beginAssetPath removeLastObject];
+                        if([beginAssetPath isEqualToArray:beginStackPath]){
+                            ret = asset;
+                            [beginAssetPath release];
+                            [beginStackPath release];
+                            break;
+                        }
                         [beginAssetPath release];
                         [beginStackPath release];
-                        break;
+
                     }
-                    [beginAssetPath release];
-                    [beginStackPath release];
-
                 }
-            }
 
+            }
         }
+        [ret retain];
     }
+    [ret autorelease];
 
     return ret;
 }
 
 -(int)parseFromData:(NSData*)data{
+    int ret = 0;
+
     @autoreleasepool {
         if (data != nil) {
-            NSString *xml = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
-            if (xml != nil) {
-                NSError *error = NULL;
-                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\s*$\\r?\\n" options:NSRegularExpressionAnchorsMatchLines error:&error];
-                xml = [regex stringByReplacingMatchesInString:xml options:0 range:NSMakeRange(0, [xml length]) withTemplate:@""];
-                data = [xml dataUsingEncoding:NSUTF8StringEncoding];
-            } else
-                return -1;
+            @autoreleasepool {
+                NSString *xml = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+                if (xml != nil) {
+                    NSError *error = NULL;
+                    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\s*$\\r?\\n" options:NSRegularExpressionAnchorsMatchLines error:&error];
+                    xml = [regex stringByReplacingMatchesInString:xml options:0 range:NSMakeRange(0, [xml length]) withTemplate:@""];
+                    data = [[xml dataUsingEncoding:NSUTF8StringEncoding] retain];
+                } else {
+                    return -1;
+                }
+            }
+            [data autorelease];
         }
         NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
-        int ret = [self startParser:parser];
+        ret = [self startParser:parser];
         [parser release];
-        return ret;
     }
+
+    return ret;
 }
 
 -(int)parseFromURL:(NSURL*)url{
@@ -186,15 +197,20 @@ static NSString *ElementStop = @"ElementStop";
 #warning Change to Async request, sometimes it blocks main thread!
         NSData *data = [NSData dataWithContentsOfURL:url];
         if (data != nil) {
-            NSString *xml = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
-            if (xml != nil) {
-                NSError *error = NULL;
-                NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\s*$\\r?\\n" options:NSRegularExpressionAnchorsMatchLines error:&error];
-                xml = [regex stringByReplacingMatchesInString:xml options:0 range:NSMakeRange(0, [xml length]) withTemplate:@""];
-                data = [xml dataUsingEncoding:NSUTF8StringEncoding];
-            } else
-                return -1;
+            @autoreleasepool {
+                NSString *xml = [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+                if (xml != nil) {
+                    NSError *error = NULL;
+                    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^\\s*$\\r?\\n" options:NSRegularExpressionAnchorsMatchLines error:&error];
+                    xml = [regex stringByReplacingMatchesInString:xml options:0 range:NSMakeRange(0, [xml length]) withTemplate:@""];
+
+                    data = [[xml dataUsingEncoding:NSUTF8StringEncoding] retain];
+                } else
+                    return -1;
+            }
+            [data autorelease];
         }
+
         NSXMLParser *parser = [[NSXMLParser alloc] initWithData:data];
         int ret = [self startParser:parser];
         [parser release];
@@ -228,86 +244,90 @@ static NSString *ElementStop = @"ElementStop";
 
 
 - (void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qualifiedName attributes:(NSDictionary *)attributeDict{
-    //NSLog(@"open=%@", elementName);
-    @synchronized(self) {
-        [mElementStack addObject:elementName];
-    }
-
-    //Check if we are looking for this asset
-    BasicParserAsset* asset = [self getAssetForElementStack:mElementStack];
-    if(asset != nil){
-        elementAttributeDict = attributeDict;//make temprary available to derived classes
-        [elementAttributeDict retain];
-
-        if([asset stringValueFunction] != nil && [asset stringValueObject] != nil){
-            //we are interested in a string and we are looking for this
-            [[asset stringCache] setString:@""];
-            //[asset setStringCache:[[[NSString alloc] init] autorelease]];
+    @autoreleasepool {
+        //NSLog(@"open=%@", elementName);
+        @synchronized(self) {
+            [mElementStack addObject:elementName];
         }
-        if([asset function] != nil && [asset functionObject] != nil){
-            if([[asset functionObject] respondsToSelector:[asset function]]){
-                [[asset functionObject] performSelector:[asset function] withObject:ElementStart];
+
+        //Check if we are looking for this asset
+        BasicParserAsset* asset = [self getAssetForElementStack:mElementStack];
+        if(asset != nil){
+            elementAttributeDict = attributeDict;//make temprary available to derived classes
+            [elementAttributeDict retain];
+
+            if([asset stringValueFunction] != nil && [asset stringValueObject] != nil){
+                //we are interested in a string and we are looking for this
+                [[asset stringCache] setString:@""];
+                //[asset setStringCache:[[[NSString alloc] init] autorelease]];
             }
-        }
+            if([asset function] != nil && [asset functionObject] != nil){
+                if([[asset functionObject] respondsToSelector:[asset function]]){
+                    [[asset functionObject] performSelector:[asset function] withObject:ElementStart];
+                }
+            }
 
+        }
     }
 }
 
 
 - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName{
-    //NSLog(@"close=%@", elementName);
-  
-    BasicParserAsset* asset = [self getAssetForElementStack:mElementStack];
-    if(asset != nil){
-        currentElementName = elementName;//make temporary available to derived classes
+    @autoreleasepool {
+        BasicParserAsset* asset = [self getAssetForElementStack:mElementStack];
+        if(asset != nil){
+            currentElementName = elementName;//make temporary available to derived classes
 
-        //We where looking for this
-        //Set string (call function to set)
-        if([asset stringValueFunction] != nil && [asset stringValueObject] != nil){
-            if([[asset stringValueObject] respondsToSelector:[asset stringValueFunction]]){
-                NSString *obj = [[NSString alloc] initWithString:[asset stringCache]];
-                [[asset stringValueObject] performSelector:[asset stringValueFunction] withObject:obj];
-                [obj release];
-            }else{
-                NSLog(@"Does not respond to selector @" );
+            //We where looking for this
+            //Set string (call function to set)
+            if([asset stringValueFunction] != nil && [asset stringValueObject] != nil){
+                if([[asset stringValueObject] respondsToSelector:[asset stringValueFunction]]){
+                    NSString *obj = [[NSString alloc] initWithString:[asset stringCache]];
+                    [[asset stringValueObject] performSelector:[asset stringValueFunction] withObject:obj];
+                    [obj release];
+                }else{
+                    NSLog(@"Does not respond to selector @" );
+                }
             }
-        }
-        //Call function
-        if([asset function] != nil && [asset functionObject] != nil){
-            if([[asset functionObject] respondsToSelector:[asset function]]){
-                [[asset functionObject] performSelector:[asset function] withObject:ElementStop];
+            //Call function
+            if([asset function] != nil && [asset functionObject] != nil){
+                if([[asset functionObject] respondsToSelector:[asset function]]){
+                    [[asset functionObject] performSelector:[asset function] withObject:ElementStop];
+                }
             }
+            elementAttributeDict = nil;
+            [elementAttributeDict release];
         }
-        elementAttributeDict = nil;
-        [elementAttributeDict release];
-    }
 
-    NSString *lastObject;
-    @synchronized(self) {
-        lastObject = [mElementStack lastObject];
-    }
-
-    if([elementName isEqualToString:lastObject]){
+        NSString *lastObject;
         @synchronized(self) {
-            [mElementStack removeLastObject];
+            lastObject = [mElementStack lastObject];
         }
-    }else{
-        //XML structure error (!)
-        NSLog(@"XML wrong formatted (!)");
-        [parser abortParsing];
+
+        if([elementName isEqualToString:lastObject]){
+            @synchronized(self) {
+                [mElementStack removeLastObject];
+            }
+        }else{
+            //XML structure error (!)
+            NSLog(@"XML wrong formatted (!)");
+            [parser abortParsing];
+        }
     }
 }
 
 - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string{
-    //The parser object may send the delegate several parser:foundCharacters: messages to report the characters of an element. 
-    //Because string may be only part of the total character content for the current element, 
-    //you should append it to the current accumulation of characters until the element changes.
+    @autoreleasepool {
+        //The parser object may send the delegate several parser:foundCharacters: messages to report the characters of an element.
+        //Because string may be only part of the total character content for the current element, 
+        //you should append it to the current accumulation of characters until the element changes.
 
-    //Are we looking for this ?
-    //Check if we are looking for this asset
-    BasicParserAsset* asset = [self getAssetForElementStack:mElementStack];
-    if(asset != nil){
-        [[asset stringCache] appendString:string];
+        //Are we looking for this ?
+        //Check if we are looking for this asset
+        BasicParserAsset* asset = [self getAssetForElementStack:mElementStack];
+        if(asset != nil){
+            [[asset stringCache] appendString:string];
+        }
     }
 }
 
--- src/upnp/SoapAction.m
+++ src/upnp/SoapAction.m
@@ -87,74 +87,77 @@
 
     _mOutput = output;//we need it as a member to fill it during parsing
 
-    //SOAP Message to Send
-    NSMutableString *body = [[NSMutableString alloc] init];
-    [body appendString:@"<?xml version=\"1.0\" encoding=\"utf-8\"?>"];
-    [body appendString:@"<s:Envelope s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"];
-    [body appendString:@"<s:Body>"];
-    [body appendFormat:@"<u:%@ xmlns:u=\"%@\">", soapAction, _upnpNameSpace];
-    for (id key in parameters) {
-        [body appendFormat:@"<%@>%@</%@>", key, parameters[key], key];
-    }
-    [body appendFormat:@"</u:%@>", soapAction];
-    [body appendFormat:@"</s:Body></s:Envelope>"];
-    len = [body length];
+    @autoreleasepool {
 
-    //Construct the HTML POST 
-    NSMutableURLRequest* urlRequest=[NSMutableURLRequest requestWithURL:_actionURL
-                                                            cachePolicy:NSURLRequestReloadIgnoringCacheData
-                                                        timeoutInterval:15.0];
-
-    [urlRequest setValue:[NSString stringWithFormat:@"\"%@#%@\"", _upnpNameSpace, soapAction] forHTTPHeaderField:@"SOAPACTION"];
-    [urlRequest setValue:[NSString stringWithFormat:@"%ld", (unsigned long)len] forHTTPHeaderField:@"CONTENT-LENGTH"];
-    [urlRequest setValue:@"text/xml;charset=\"utf-8\"" forHTTPHeaderField:@"CONTENT-TYPE"];
-
-    /*
-    [urlRequest setValue:@"" forHTTPHeaderField:@"Accept-Language"];
-    [urlRequest setValue:@"" forHTTPHeaderField:@"Accept-Encoding"];
-    */
-
-    //POST (Synchronous)
-    [urlRequest setHTTPMethod:@"POST"];
-    [urlRequest setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
-
-
-    NSHTTPURLResponse *urlResponse;
-    NSError *error = nil;
-    NSData *resp = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:&urlResponse error:&error];
-    self.response = urlResponse;
-    self.error = error;
-
-    // Check the Server Return Code @TODO
-    if ([urlResponse statusCode] != 200) {
-        ret = -[urlResponse statusCode];
-        NSString *rsp = [[NSString  alloc] initWithData:resp encoding:NSUTF8StringEncoding];
-        NSLog(@"[UPnP] Error (SoapAction): Got a non 200 response: %ld. Data: %@", (long)[urlResponse statusCode], rsp);
-        [rsp release];
-        if (ret == 0) {
-            ret = -408; // Why 408?
+        //SOAP Message to Send
+        NSMutableString *body = [[NSMutableString alloc] init];
+        [body appendString:@"<?xml version=\"1.0\" encoding=\"utf-8\"?>"];
+        [body appendString:@"<s:Envelope s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\">"];
+        [body appendString:@"<s:Body>"];
+        [body appendFormat:@"<u:%@ xmlns:u=\"%@\">", soapAction, _upnpNameSpace];
+        for (id key in parameters) {
+            [body appendFormat:@"<%@>%@</%@>", key, parameters[key], key];
         }
-    }
-    else {
-        ret = 0;
-    }
+        [body appendFormat:@"</u:%@>", soapAction];
+        [body appendFormat:@"</s:Body></s:Envelope>"];
+        len = [body length];
 
-    if (ret == 0 && [resp length] > 0 ) {
-        //Parse result
-        //Clear the assets becuase the action can be re-used
-        [self clearAllAssets];
-        NSString *responseGroupTag = [NSString stringWithFormat:@"%@Response", soapAction];
-        for (id key in output) {
-            [self addAsset:@[@"Envelope", @"Body", responseGroupTag, (NSString*)key] callfunction:nil functionObject:nil setStringValueFunction:@selector(setStringValueForFoundAsset:) setStringValueObject:self];
+        //Construct the HTML POST 
+        NSMutableURLRequest* urlRequest=[NSMutableURLRequest requestWithURL:_actionURL
+                                                                cachePolicy:NSURLRequestReloadIgnoringCacheData
+                                                            timeoutInterval:60.0];
+
+        [urlRequest setValue:[NSString stringWithFormat:@"\"%@#%@\"", _upnpNameSpace, soapAction] forHTTPHeaderField:@"SOAPACTION"];
+        [urlRequest setValue:[NSString stringWithFormat:@"%ld", (unsigned long)len] forHTTPHeaderField:@"CONTENT-LENGTH"];
+        [urlRequest setValue:@"text/xml;charset=\"utf-8\"" forHTTPHeaderField:@"CONTENT-TYPE"];
+
+        /*
+        [urlRequest setValue:@"" forHTTPHeaderField:@"Accept-Language"];
+        [urlRequest setValue:@"" forHTTPHeaderField:@"Accept-Encoding"];
+        */
+
+        //POST (Synchronous)
+        [urlRequest setHTTPMethod:@"POST"];
+        [urlRequest setHTTPBody:[body dataUsingEncoding:NSUTF8StringEncoding]];
+
+
+        NSHTTPURLResponse *urlResponse;
+        NSError *error = nil;
+        NSData *resp = [NSURLConnection sendSynchronousRequest:urlRequest returningResponse:&urlResponse error:&error];
+        self.response = urlResponse;
+        self.error = error;
+
+        // Check the Server Return Code @TODO
+        if ([urlResponse statusCode] != 200) {
+            ret = -[urlResponse statusCode];
+            NSString *rsp = [[NSString  alloc] initWithData:resp encoding:NSUTF8StringEncoding];
+            NSLog(@"[UPnP] Error (SoapAction): Got a non 200 response: %ld. Data: %@", (long)[urlResponse statusCode], rsp);
+            [rsp release];
+            if (ret == 0) {
+                ret = -408; // Why 408?
+            }
+        }
+        else {
+            ret = 0;
         }
 
-        //uShare Issues here, can not handle names like 'Bj~rk
-        ret = [super parseFromData:resp];
+        if (ret == 0 && [resp length] > 0 ) {
+            //Parse result
+            //Clear the assets becuase the action can be re-used
+            [self clearAllAssets];
+            NSString *responseGroupTag = [NSString stringWithFormat:@"%@Response", soapAction];
+            for (id key in output) {
+                [self addAsset:@[@"Envelope", @"Body", responseGroupTag, (NSString*)key] callfunction:nil functionObject:nil setStringValueFunction:@selector(setStringValueForFoundAsset:) setStringValueObject:self];
+            }
+
+            //uShare Issues here, can not handle names like 'Bj~rk
+            ret = [super parseFromData:resp];
+        }
+
+        [body release];
+
+        _mOutput = nil;
     }
-
-    [body release];
-
-    _mOutput = nil;
 
     return ret;
 }
